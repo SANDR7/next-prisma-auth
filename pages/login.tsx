561092ee3186cb09f5efbbe3f10502057bf417ca
Sander <sandervanast1@gmail.com>
2022-03-27 15:00:01 +0200

    forms

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,12 +1,47 @@
-import PageContainer from '@/layout/Main'
-import React from 'react'
+import PageContainer from '@/layout/Main';
+import {
+  Anchor,
+  Button,
+  Center,
+  Checkbox,
+  Divider,
+  Group,
+  Paper,
+  Text,
+  PasswordInput,
+  TextInput
+} from '@mantine/core';
+import React from 'react';
 
 const login = () => {
   return (
-	<PageContainer>
-		login
-	</PageContainer>
-  )
-}
+    <PageContainer>
+      <Center mt={40}>
+        <Paper style={{width: '50%'}}>
+          <TextInput label="Email" placeholder="Your email" required />
+          <PasswordInput
+            label="Password"
+            placeholder="Your password"
+			required
+            mt="md"
+          />
+          <Group position="apart" mt="md">
+            {/* <Checkbox label="Remember me" /> */}
+            <Anchor<'a'>
+              onClick={(event) => event.preventDefault()}
+              href="#"
+              size="sm"
+            >
+              Forgot password?
+            </Anchor>
+          </Group>
+          <Button fullWidth mt="xl">
+            Log in
+          </Button>
+        </Paper>
+      </Center>
+    </PageContainer>
+  );
+};
 
-export default login
+export default login;
--- pages/register.tsx
+++ pages/register.tsx
@@ -1,10 +1,25 @@
 import PageContainer from '@/layout/Main'
+import { Center, Paper, TextInput, PasswordInput, Group, Button, Anchor } from '@mantine/core'
 import React from 'react'
 
 const register = () => {
   return (
 	<PageContainer>
-	Register	
+	  <Center mt={40}>
+        <Paper style={{width: '50%'}}>
+          <TextInput label="Username" placeholder='Your username' required />
+          <TextInput label="Email" placeholder="Your email" mt="md" required />
+          <PasswordInput
+            label="Password"
+            placeholder="Your password"
+            mt="md"
+          />
+         
+          <Button fullWidth mt="xl">
+            Log in
+          </Button>
+        </Paper>
+      </Center>
 	</PageContainer>
   )
 }
