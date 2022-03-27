7f092319cfc065307c1344cf38529ae2e15ea9df
Sander <sandervanast1@gmail.com>
2022-03-27 14:25:14 +0200

    Heading navigation

--- layout/Navigation.tsx
+++ layout/Navigation.tsx
@@ -1,15 +1,39 @@
-import { Container, Header, Navbar } from '@mantine/core';
+import {
+  Anchor,
+  Box,
+  Button,
+  Container,
+  Group,
+  Header,
+  Navbar
+} from '@mantine/core';
+import Link from 'next/link';
 import React from 'react';
 
 export const Navigation = () => {
-  return <Navbar width={{base: 300}}>Navigation</Navbar>;
+  return <Navbar width={{ base: 300 }}>Navigation</Navbar>;
 };
 
 export const Heading = () => {
+  const PageItem = ({ label, link }: { label: string; link: string }) => (
+    <Anchor component={Link} href={link}>
+      <Button variant="light" compact>
+        {label}
+      </Button>
+    </Anchor>
+  );
   return (
-    <Header height={60} p="md">
-      <Container>
-      Heading
+    <Header height={60} p="lg">
+      <Container style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+        <Box>
+          The App
+        </Box>
+        <Group position='right'>
+          <PageItem label="Home" link="/" />
+          {/* <PageItem label="Login" link="/login" /> */}
+          {/* <PageItem label="Register" link="/register" /> */}
+          <PageItem label="About" link="/#" />
+        </Group>
       </Container>
     </Header>
   );
