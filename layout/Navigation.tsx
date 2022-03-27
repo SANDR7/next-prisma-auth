1fddb321a10da1b08ae92dc60ae1285fbd270156
Sander <sandervanast1@gmail.com>
2022-03-27 14:37:28 +0200

    dark theme toggle

--- layout/Navigation.tsx
+++ layout/Navigation.tsx
@@ -1,20 +1,26 @@
 import {
+  ActionIcon,
   Anchor,
   Box,
   Button,
   Container,
   Group,
   Header,
-  Navbar
+  Navbar,
+  useMantineColorScheme
 } from '@mantine/core';
 import Link from 'next/link';
 import React from 'react';
+import { Moon, Sun } from 'tabler-icons-react';
 
 export const Navigation = () => {
   return <Navbar width={{ base: 300 }}>Navigation</Navbar>;
 };
 
 export const Heading = () => {
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+  const dark = colorScheme === 'dark';
+
   const PageItem = ({ label, link }: { label: string; link: string }) => (
     <Anchor component={Link} href={link}>
       <Button variant="light" compact>
@@ -22,17 +28,33 @@ export const Heading = () => {
       </Button>
     </Anchor>
   );
+
+  // Static nav
+  // return user ? account nav : static nav
   return (
     <Header height={60} p="lg">
-      <Container style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
-        <Box>
-          The App
-        </Box>
-        <Group position='right'>
+      <Container
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center'
+        }}
+      >
+        <Box>The App</Box>
+        <Group position="right">
           <PageItem label="Home" link="/" />
           {/* <PageItem label="Login" link="/login" /> */}
           {/* <PageItem label="Register" link="/register" /> */}
           <PageItem label="About" link="/#" />
+          <ActionIcon
+            variant="outline"
+            ml={4}
+            color={dark ? 'yellow' : 'blue'}
+            onClick={() => toggleColorScheme()}
+            title="Toggle color scheme"
+          >
+            {dark ? <Sun size={18} /> : <Moon size={18} />}
+          </ActionIcon>
         </Group>
       </Container>
     </Header>
