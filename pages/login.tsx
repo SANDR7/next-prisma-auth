02b840f49a746bc29d0cf6c5865aa04f08882d4c
Sander van Ast <sandervanast1@gmail.com>
2022-03-27 13:10:15 +0200

    Basic pages

---
+++ pages/login.tsx
@@ -0,0 +1,12 @@
+import PageContainer from '@/layout/Main'
+import React from 'react'
+
+const login = () => {
+  return (
+	<PageContainer>
+		login
+	</PageContainer>
+  )
+}
+
+export default login
---
+++ pages/register.tsx
@@ -0,0 +1,12 @@
+import PageContainer from '@/layout/Main'
+import React from 'react'
+
+const register = () => {
+  return (
+	<PageContainer>
+	Register	
+	</PageContainer>
+  )
+}
+
+export default register
