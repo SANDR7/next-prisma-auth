2bd901878be88a5175080afddffced5304e0c871
Sander van Ast <sandervanast1@gmail.com>
2022-03-27 13:10:25 +0200

    basic navigation component

---
+++ layout/Navigation.tsx
@@ -0,0 +1,14 @@
+import { Header, Navbar } from '@mantine/core';
+import React from 'react';
+
+export const Navigation = () => {
+  return <Navbar width={{base: 300}}>Navigation</Navbar>;
+};
+
+export const Heading = () => {
+  return (
+    <Header height={60} p="md">
+      Heading
+    </Header>
+  );
+};
